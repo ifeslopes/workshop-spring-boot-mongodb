e7314ccb3288ed4bf8679b853d9ee4563b155780
ifeslopes <ifes.lopes@gmail.com>
2021-01-20 11:57:14 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/lopes/workshopmongo/domain/Post.java
+++ src/main/java/com/lopes/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.lopes.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.lopes.workshopmongo.dto.AuthorDTO;
+import com.lopes.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +76,15 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	
 
 	@Override
 	public int hashCode() {
@@ -97,6 +111,7 @@ public class Post implements Serializable {
 		return true;
 	}
 
+	
 	
 	
 
